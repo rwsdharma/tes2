213b850544c1020c3b3c0171ed817d5f7de41871
rwsdharma <92496268+rwsdharma@users.noreply.github.com>
2021-11-19 10:23:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let stars = document.getElementById("stars")
+let stars = document.getElementById('stars');
 let moon = document.getElementById('moon')
 let mountains_behind = document.getElementById('mountains_behind')
 let text = document.getElementById('text')
@@ -7,8 +7,8 @@ let mountains_front = document.getElementById('mountains_front');
             
 window.addEventListener('scroll', function() {
 let value = window.scrollY;
-stars.style.left = value * 0.25 + 'px';
-moon.style.top = value * 1.25 + 'px';
+stars.style.left = value * 0.3 + 'px';
+moon.style.top = value * 0.25 + 'px';
 mountains_behind.style.top = value * 0.5 + 'px';
 mountains_front.style.top = value * 0 + 'px';
 text.style.marginRight = value * 3 + 'px';
